78d1da2e32194e3c2444c17e10a543a1bec7e053
Cristian <salutplayermd@gmail.com>
2026-08-12 18:19:50 +0300

    Added atomic

--- main.cpp
+++ main.cpp
@@ -1,30 +1,27 @@
+#include <atomic>
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <mutex>
 
-std::mutex lock;
-static int counterValue = 100;
+static std::atomic<int> counterValue = 0;
+
 void updateCounterValue() {
-  lock.lock();
-    counterValue += 1;
-    std::cout << "counterValue: " << counterValue << std::endl;
-  lock.unlock();
+  std::cout << "counterValue: " << counterValue << std::endl;
 }
 
 int main() {
-  
-auto lambda =[](int x) {
-  std::cout<<"Hello from thread "<<std::this_thread::get_id()<<std::endl;
-  std::cout<<"Argument passed in : "<<x<<std::endl;
-};
+
+  auto lambda = [](int x) {
+    std::cout << "Hello from thread " << std::this_thread::get_id() << std::endl;
+    std::cout << "Argument passed in : " << x << std::endl;
+  };
   std::vector<std::jthread> jThreads;
   for (int i = 0; i < 10; i++) {
     jThreads.emplace_back(updateCounterValue);
   }
-  for (auto& jThread : jThreads) {
+  for (auto &jThread : jThreads) {
     jThread.join();
   }
-  std::cout<<"Hello from main thread"<<std::endl;
+  std::cout << "Hello from main thread" << std::endl;
   return 0;
 }
