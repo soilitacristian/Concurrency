85cffe62b9950a5f525bb226ccd093c44c2fb3f4
Cristian <soilitacristian@gmail.com>
2026-08-24 17:52:18 +0300

    condition variable and unique lock

--- main.cpp
+++ main.cpp
@@ -1,80 +1,33 @@
-#include <atomic>
 #include <iostream>
 #include <mutex>
 #include <thread>
-#include <vector>
+#include <condition_variable>
+#include <queue>
 
-std::mutex lock;
-std::atomic<int> sharedJob{0};
+std::mutex mtx;
+std::condition_variable cv;
+std::queue<int> queue;
 
-int calculateSumValue(const std::vector<int>& numbers)
-{
-    int sum{};
-
-    for (auto number : numbers)
-        sum += number;
-
-    return sum;
-}
-
-void calculateSquareValue(int number, std::vector<int>& results)
-{
-    int result = number * number;
-
-    {
-        std::lock_guard guard(lock);
-
-        results.push_back(result);
-
-        std::cout
-            << "Thread " << std::this_thread::get_id()
-            << " calculated "
-            << number << " * " << number
-            << " = " << result
-            << std::endl;
+void push(){
+    for (int i = 0; i < 10000; ++i){
+        std::unique_lock<std::mutex> lock(mtx);
+        queue.push(i);
+        std::cout<<"Pushed : "<<i<<std::endl;
+        cv.notify_one();
     }
 }
-
-void workerFunction(const std::vector<int>& numbers,
-                    std::vector<int>& results)
-{
-    while (true)
-    {
-        int index = sharedJob++;
-
-        if (index >= numbers.size())
-            break;
-
-        calculateSquareValue(numbers[index], results);
+void pop(){
+    while (true){
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [] {return !queue.empty();});
+        int val = queue.front();
+        queue.pop();
+        std::cout<<"Popped : "<<val<<std::endl;
     }
 }
-
-int main()
-{
-    std::vector<std::jthread> jThreads;
-
-    std::vector<int> numbers = {
-        5, 8, 12, 20, 3, 7, 11, 15
-    };
-
-    std::vector<int> results;
-
-    for (int i = 0; i < 2; i++)
-    {
-        jThreads.emplace_back(
-            workerFunction,
-            std::cref(numbers),
-            std::ref(results)
-        );
-    }
-
-    for (auto& jThread : jThreads)
-    {
-        jThread.join();
-    }
-
-    std::cout
-        << "Sum = "
-        << calculateSumValue(results)
-        << std::endl;
+int main(){
+    std::jthread t1(push);
+    std::jthread t2(pop);
+    
+    return 0;
 }
